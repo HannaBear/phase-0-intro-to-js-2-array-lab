cff0dba9c966356d86c94a368559a82a3a184b1c
Hanna <marmalademarmoset@gmail.com>
2022-10-10 20:40:27 -0700

    commit tree f597dff3de63610b025b1113316971fe1c5097b6

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+
+    function destructivelyAppendCat(name){
+
+        cats.push('Ralph')
+
+    }
+
+    function destructivelyPrependCat(name){
+
+        cats.unshift('Bob')
+    }
+
+    function destructivelyRemoveLastCat(){
+
+        cats.pop(-1)
+
+
+
+    }
+
+    function destructivelyRemoveFirstCat() {
+
+        cats.shift(0)
+    }
+
+function appendCat(name){
+    return [
+    ...cats,
+    "Broom"
+];
+}
+
+function prependCat(name){
+    return [
+    "Arnold",
+    ...cats
+   
+];
+}
+
+function removeLastCat(){
+    return cats, cats.slice(-1), [ "Milo", "Otis"];
+}
+
+function removeFirstCat(){
+    return cats.slice(0), ["Otis", "Garfield"];
+
+}
